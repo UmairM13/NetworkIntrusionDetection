5b998b749f455146a619a6e87821dade1ef38c52
UmairM13 <22493168@stu.mmu.ac.uk>
2024-10-21 23:10:15 +0100

    added CORS

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from app.routes.user_routes import auth_bp
 from app.routes.nids_routes import nids_bp
 import os
 
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.secret_key = os.urandom(24).hex()
 
